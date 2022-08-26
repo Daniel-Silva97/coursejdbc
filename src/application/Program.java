74171270a3bd182d39a57c333ad244f3fccdae75
Daniel da silva pedro <danielsp699@outlook.com>
2022-08-26 18:22:17 -0300

    JDBC - Select table in Database

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,12 +3,29 @@ package application;
 import db.DB;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Program {
     public static void main(String[] args) {
-
         Connection conn = DB.getConnection();
+        Statement statement = null;
+        ResultSet resultSet = null;
+        try{
+            conn = DB.getConnection();
+            statement = conn.createStatement();
+            resultSet = statement.executeQuery("select * from department");
 
-        DB.closeConnection();
+            while (resultSet.next()){
+                System.out.println(resultSet.getInt("Id") + ", " + resultSet.getString("Name"));
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+        } finally {
+            DB.closeStatement(statement);
+            DB.closeResultSet(resultSet);
+            DB.closeConnection();
+        }
     }
 }
