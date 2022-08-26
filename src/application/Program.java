3336f457c3e978260b8567ea835c3fb0f70649e4
Daniel da silva pedro <danielsp699@outlook.com>
2022-08-26 18:48:57 -0300

    JDBC - UPDATE in Database

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,45 +8,23 @@ import java.text.SimpleDateFormat;
 
 public class Program {
     public static void main(String[] args) {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
+        Connection conn = null;
+        PreparedStatement statement = null;
         try {
-            connection = DB.getConnection();
-//            preparedStatement = connection.prepareStatement(
-//                    "INSERT INTO seller" +
-//                            "(Name, Email, BirthDate, BaseSalary, DepartmentId)" +
-//                            "VALUES" +
-//                            "(?, ?, ?, ?, ?)",
-//                    Statement.RETURN_GENERATED_KEYS);
-//
-//            preparedStatement.setString(1, "Carl Purple");
-//            preparedStatement.setString(2, "carl@gmail.com");
-//            preparedStatement.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
-//            preparedStatement.setDouble(4, 3000.0);
-//            preparedStatement.setInt(5, 4);
-
-            preparedStatement = connection.prepareStatement(
-                    "INSERT INTO department" +
-                    "(Name) " +
-                    "VALUES ('D1'),('D2')",
-                    Statement.RETURN_GENERATED_KEYS);
-
-            int rowsAffected = preparedStatement.executeUpdate();
-            if (rowsAffected > 0){
-                ResultSet resultSet = preparedStatement.getGeneratedKeys();
-                while (resultSet.next()){
-                    int id = resultSet.getInt(1);
-                    System.out.println("Done! ID = " + id);
-                }
-            } else {
-                System.out.println("No rows affected! ");
-            }
-
+            conn = DB.getConnection();
+            statement = conn.prepareStatement(
+                    "UPDATE seller " +
+                            "SET BaseSalary = BaseSalary + ? " +
+                            "WHERE " +
+                            "(DepartmentId = ?)");
+            statement.setDouble(1, 200.0);
+            statement.setInt(2, 2);
+            int rowAffected = statement.executeUpdate();
+            System.out.println("Done! Rows Affected: " + rowAffected);
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
-            DB.closeStatement(preparedStatement);
+            DB.closeStatement(statement);
             DB.closeConnection();
         }
     }
